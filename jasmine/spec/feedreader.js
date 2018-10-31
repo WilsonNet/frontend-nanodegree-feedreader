d29a5b466af5789c866100a11518b77e0bb840bb
Wilson Neto <wilsonpsn@hotmail.com>
2018-10-31 18:10:41 -0300

    Tested if menu is hidden by default

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -35,7 +35,7 @@ $(function() {
             allFeeds.forEach(obj=>{
                 expect(obj.url).toBeDefined();
                 expect(obj.url.length).not.toBe(0);
-            })
+            });
         });
 
 
@@ -47,7 +47,7 @@ $(function() {
             allFeeds.forEach(obj=>{
                 expect(obj.name).toBeDefined();
                 expect(obj.name.length).not.toBe(0);
-            })
+            });
         });
         
 
@@ -55,12 +55,17 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function(){
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+
+         it('menu element is hidden by default', function(){
+             expect($('body').hasClass('menu-hidden')).toBe(true);
+
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
@@ -83,4 +88,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
